d1ba4740d98c4d62a59e3e43cb3d522354b05b51
mhysa123 <987649836@qq.com>
2018-01-26 20:42:22 +0800

    pythonTask

---
+++ Task1.py
@@ -0,0 +1,42 @@
+"""
+
+Intro to Python Project 1, Task 1
+
+Complete each task in the file for that task. Submit the whole folder
+as a zip file or GitHub repo. 
+Full submission instructions are available on the Project Preparation page.
+"""
+
+
+"""
+Read file into texts and calls. 
+It's ok if you don't understand how to read files
+You will learn more about reading files in future lesson
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+TASK 1: 
+How many different telephone numbers are there in the records? 
+Print a message: 
+"There are <count> different telephone numbers in the records."
+"""
+telnumsSet = set()
+#record different number list
+for index in range(len(texts)+len(calls)):
+    if index < len(texts):
+        telnumsSet.add(texts[index][0])
+        telnumsSet.add(texts[index][1])
+    else:
+        telnumsSet.add(calls[index-len(texts)][0])
+        telnumsSet.add(calls[index-len(texts)][1])
+print("There are {} different telephone numbers in the records.".format(len(telnumsSet)))
+
---
+++ Task4.py
@@ -0,0 +1,42 @@
+"""
+Intro to Python Lab 1, Task 4
+
+Complete each task in the file for that task. Submit the whole folder
+as a zip file or GitHub repo. 
+Full submission instructions are available on the Lab Preparation page.
+"""
+
+"""
+Read file into texts and calls. 
+It's ok if you don't understand how to read files
+You will learn more about reading files in future lesson
+"""
+import csv
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 4:
+The telephone company want to identify numbers that might be doing
+telephone marketing. Create a set of possible telemarketers: 
+these are numbers that make outgoing calls but never send texts,
+receive texts or receive incoming calls.
+
+Print a message: 
+"These numbers could be telemarketers: "
+<list of numbers>
+The list of numbers should be print out one per line in lexicographic order with no duplicates.
+"""
+telemarketers = []
+for call in calls:
+    if str(call[0]).startswith("140"):
+        telemarketers.append(call[0])
+print("These numbers could be telemarketers: {}{}".format("\n","\n".join(sorted(set(telemarketers)))))
+
+
